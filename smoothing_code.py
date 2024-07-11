Fixed npix_out bug and the masking of bad values in the output map

smoothing_code.py
@@ -89,7 +89,7 @@ def smooth_map(I, Q, U, fwhm_out=1.0, Bl=None, lmax=None, nside_out=None):
     # SETUP INPUTS 
     if isinstance(nside_out, type(None)):
         nside_out = NSIDE
-        npix_out = 12*nside_out**2
+    npix_out = 12*nside_out**2
 
     if isinstance(lmax, type(None)):
         lmax = 3 * NSIDE - 1
@@ -122,9 +122,12 @@ def smooth_map(I, Q, U, fwhm_out=1.0, Bl=None, lmax=None, nside_out=None):
     dalm_B = hp.almxfl(alm_B, bl_raw_spin2[:lmax + 1])
 
     dI, dQ, dU = hp.alm2map((dalm_I, dalm_E, dalm_B), nside=nside_out)
-    dI[I[0] == hp.UNSEEN] = hp.UNSEEN
-    dQ[Q[1] == hp.UNSEEN] = hp.UNSEEN
-    dU[U[2] == hp.UNSEEN] = hp.UNSEEN
+
+    II_mask = hp.ud_grade(I, nside_out) == hp.UNSEEN
+
+    dI[II_mask] = hp.UNSEEN
+    dQ[II_mask] = hp.UNSEEN
+    dU[II_mask] = hp.UNSEEN
 
     return dI, dQ, dU
 
@@ -201,7 +204,8 @@ def smooth_variance_map(II, QQ, UU, IQ=None, IU=None, QU=None, fwhm_out=1.0,  Bl
 
     if isinstance(nside_out, type(None)):
         nside_out = NSIDE
-        npix_out = 12*nside_out**2
+    npix_out = 12*nside_out**2
+    
 
     # SETUP INPUTS 
     if isinstance(lmax, type(None)):
@@ -251,18 +255,127 @@ def smooth_variance_map(II, QQ, UU, IQ=None, IU=None, QU=None, fwhm_out=1.0,  Bl
     dIU = np.zeros(npix_out)
     dIQ = np.zeros(npix_out)
 
-    dII[II == hp.UNSEEN] = hp.UNSEEN
-    dQQ[QQ == hp.UNSEEN] = hp.UNSEEN
-    dUU[UU == hp.UNSEEN] = hp.UNSEEN
-    dIQ[IQ == hp.UNSEEN] = hp.UNSEEN
-    dIU[IU == hp.UNSEEN] = hp.UNSEEN
-    dQU[QU == hp.UNSEEN] = hp.UNSEEN
+    II_mask = hp.ud_grade(II, nside_out) == hp.UNSEEN
 
-    dII[dII != hp.UNSEEN] *= PIXAREA * (2*np.pi)
-    dQQ[dQQ != hp.UNSEEN] *= PIXAREA * (2*np.pi)
-    dUU[dUU != hp.UNSEEN] *= PIXAREA * (2*np.pi)
-    dIQ[dIQ != hp.UNSEEN] *= PIXAREA * (2*np.pi)
-    dIU[dIU != hp.UNSEEN] *= PIXAREA * (2*np.pi)
-    dQU[dQU != hp.UNSEEN] *= PIXAREA * (2*np.pi)
+    dII[II_mask] = hp.UNSEEN
+    dQQ[II_mask] = hp.UNSEEN
+    dUU[II_mask] = hp.UNSEEN
+    dIQ[II_mask] = hp.UNSEEN
+    dIU[II_mask] = hp.UNSEEN
+    dQU[II_mask] = hp.UNSEEN
+
+    dII[~II_mask] *= PIXAREA * (2*np.pi)
+    dQQ[~II_mask] *= PIXAREA * (2*np.pi)
+    dUU[~II_mask] *= PIXAREA * (2*np.pi)
+    dIQ[~II_mask] *= PIXAREA * (2*np.pi)
+    dIU[~II_mask] *= PIXAREA * (2*np.pi)
+    dQU[~II_mask] *= PIXAREA * (2*np.pi)
+
+    return dII, dQQ, dUU, dIQ, dIU, dQU 
+
+
+def smooth_variance_map_with_rotation(II, QQ, UU, IQ=None, IU=None, QU=None, fwhm_out=1.0,  Bl=None, lmax=None, nside_out=None):
+    """
+    Smooth input variance maps.
+
+    Smooths two versions of the variance map, one in the original coordinate system and one rotated 90 degrees. 
+    The two maps are then combined to produce the final variance map
+
+    Parameters
+    ----------
+    II : array-like
+        The input II map.
+    QQ : array-like
+        The input QQ map.
+    UU : array-like
+        The input UU map.
+    IQ : array-like (optional)
+        The input IQ map.
+    IU : array-like (optional)
+        The input IU map.
+    QU : array-like (optional)
+        The input QU map.
+    fwhm_out : float
+        The output resolution in radians.
+    Bl : array-like
+        The input beam transfer function. 
+        Polarised B_l has l=0, and l=1 terms set to zero.
+        C_l = B_l^2 C_l^true. 
+        If None, it is assumed fwhm_in is given.
+    """
+
+    # Constants
+    NPIX = len(II)
+    NSIDE = hp.npix2nside(NPIX)
+    PIXAREA = hp.nside2pixarea(NSIDE)
+
+    if isinstance(nside_out, type(None)):
+        nside_out = NSIDE
+    npix_out = 12*nside_out**2
+    
+
+    # SETUP INPUTS 
+    if isinstance(lmax, type(None)):
+        lmax = 3 * nside_out - 1
+
+    if isinstance(Bl, type(None)):
+        Bl = hp.gauss_beam(fwhm_out, lmax=lmax)
+
+    pixel_window = hp.pixwin(nside_out)[:lmax + 1]
+    Bl[:lmax + 1] *= pixel_window
+
+    # Richard's code 
+    theta = np.linspace(0, np.pi, 1000)
+    beam = hp.bl2beam(Bl/Bl[0], theta)
+    p0l, d2p0l = get_polynomials(theta, lmax)
+    transformer = Legendre(theta = theta,
+                    beam = beam**2,
+                    lmax = lmax,
+                    p0l = p0l,
+                    d2p0l = d2p0l)
+    bl_raw_spin0 = transformer.spin0_transform()
+    bl_raw_spin2 = transformer.spin2_transform()
+    nl = transformer.get_normalisation()
+    bl_raw_spin0 /= nl
+    bl_raw_spin2 /= nl
+
+
+    alm_I = hp.map2alm(II, lmax=lmax)
+    alm_E = hp.map2alm(QQ, lmax=lmax)
+    alm_B = hp.map2alm(UU, lmax=lmax)
+    dalm_I = hp.almxfl(alm_I, bl_raw_spin0[:lmax + 1])
+    dalm_E = hp.almxfl(alm_E, bl_raw_spin0[:lmax + 1])
+    dalm_B = hp.almxfl(alm_B, bl_raw_spin0[:lmax + 1])
+    dII = hp.alm2map(dalm_I, nside=nside_out)
+    dQQ = hp.alm2map(dalm_E, nside=nside_out)
+    dUU = hp.alm2map(dalm_B, nside=nside_out)
+
+    if not isinstance(IQ, type(None)):
+        null_map = np.zeros_like(II)
+        alm_QU_E, alm_QU_B = hp.map2alm_spin([QU,null_map], spin=2,lmax=lmax)
+        dalm_QU_E = hp.almxfl(alm_QU_E, bl_raw_spin2[:lmax + 1])
+        dalm_QU_B = hp.almxfl(alm_QU_B, bl_raw_spin2[:lmax + 1])
+        dQU,_ = hp.alm2map_spin((dalm_QU_E,dalm_QU_B), spin=2,lmax=lmax, nside=nside_out)
+    else:
+        dQU = np.zeros(npix_out) 
+
+    dIU = np.zeros(npix_out)
+    dIQ = np.zeros(npix_out)
+
+    II_mask = hp.ud_grade(II, nside_out) == hp.UNSEEN
+
+    dII[II_mask] = hp.UNSEEN
+    dQQ[II_mask] = hp.UNSEEN
+    dUU[II_mask] = hp.UNSEEN
+    dIQ[II_mask] = hp.UNSEEN
+    dIU[II_mask] = hp.UNSEEN
+    dQU[II_mask] = hp.UNSEEN
+
+    dII[~II_mask] *= PIXAREA * (2*np.pi)
+    dQQ[~II_mask] *= PIXAREA * (2*np.pi)
+    dUU[~II_mask] *= PIXAREA * (2*np.pi)
+    dIQ[~II_mask] *= PIXAREA * (2*np.pi)
+    dIU[~II_mask] *= PIXAREA * (2*np.pi)
+    dQU[~II_mask] *= PIXAREA * (2*np.pi)
 
     return dII, dQQ, dUU, dIQ, dIU, dQU 

test.py
@@ -89,26 +89,9 @@ def test_smooth_variance_map():
     
     fwhm_out = np.radians(15)
     
-    #smooth_IIn, smooth_QQn, smooth_UUn, smooth_IQn, smooth_IUn, smooth_QUn = smooth_variance_map_numerical(II, QQ, UU, IQ, IU, QU, fwhm_out)
-    # smooth_IIn, smooth_QQn, smooth_UUn, smooth_IQn, smooth_IUn, smooth_QUn = hp.read_map('test_images/numerical_smooth_variances.fits',field=[0,1,2,3,4,5])
-    # pyplot.figure(figsize=(8,16))
-    # hp.mollview(smooth_IIn, title='numerical smooth_II',sub=(3,1,1))
-    # hp.mollview(smooth_QQn, title='numerical smooth_QQ',sub=(3,1,2))
-    # hp.mollview(smooth_UUn, title='numerical smooth_UU',sub=(3,1,3))
-    # hp.graticule()
-    # pyplot.savefig('test_images/numerical_smooth_variance_map.png')
-    # pyplot.close()
-    #hp.write_map('test_images/numerical_smooth_variances.fits',
-    #             [smooth_IIn, smooth_QQn, smooth_UUn, smooth_IQn, smooth_IUn, smooth_QUn],overwrite=True)
-
     smooth_II, smooth_QQ, smooth_UU, smooth_IQ, smooth_IU, smooth_QU = smooth_variance_map(
         II, QQ, UU, IQ, IU, QU, fwhm_out, nside_out=32
     )
-
-    #pyplot.plot(smooth_II, smooth_IIn, '.')
-    #pyplot.plot(smooth_QQ, smooth_QQn, '.')
-    #pyplot.plot(smooth_UU, smooth_UUn, '.')
-    #pyplot.show()
     
     assert len(smooth_II) == npix
     assert len(smooth_QQ) == npix
@@ -117,11 +100,6 @@ def test_smooth_variance_map():
     assert len(smooth_IU) == npix
     assert len(smooth_QU) == npix
     
-    # Check if smoothing reduces the standard deviation
-    #assert np.std(smooth_II) < np.std(II)
-    #assert np.std(smooth_QQ) < np.std(QQ)
-    #assert np.std(smooth_UU) < np.std(UU)
-
     hp.write_map('test_images/analytical_smooth_variances.fits',
                  [smooth_II,smooth_QQ,smooth_UU,smooth_IQ,smooth_IU,smooth_QU],overwrite=True)
     pyplot.figure(figsize=(8,16))
@@ -141,11 +119,53 @@ def test_smooth_variance_map():
     pyplot.close()
 
 
+def test_smooth_real_variance_map():
+    
+    II,QQ,UU,QU = hp.read_map('cbass_maps/cbass_DR1_ss_1024.fits',field=[3,4,5,6])
+    IU = np.zeros_like(II)
+    IQ = np.zeros_like(II)
+    npix = II.size
+    nside = hp.npix2nside(npix)
+    nside_out = 32
+    fwhm_out = np.radians(15)
+    
+    smooth_II, smooth_QQ, smooth_UU, smooth_IQ, smooth_IU, smooth_QU = smooth_variance_map(
+        II, QQ, UU, IQ, IU, QU, fwhm_out, nside_out=32
+    )
+    npix_out = hp.nside2npix(nside_out)
+    
+    assert len(smooth_II) == npix_out
+    assert len(smooth_QQ) == npix_out
+    assert len(smooth_UU) == npix_out
+    assert len(smooth_IQ) == npix_out
+    assert len(smooth_IU) == npix_out
+    assert len(smooth_QU) == npix_out
+    
+    hp.write_map('test_images/real_analytical_smooth_variances.fits',
+                 [smooth_II,smooth_QQ,smooth_UU,smooth_IQ,smooth_IU,smooth_QU],overwrite=True)
+    pyplot.figure(figsize=(8,16))
+    hp.mollview(smooth_II, title='smooth_II',sub=(3,1,1),rot=[0,90])
+    hp.mollview(smooth_QQ, title='smooth_QQ',sub=(3,1,2),rot=[0,90])
+    hp.mollview(smooth_UU, title='smooth_UU',sub=(3,1,3),rot=[0,90])
+    hp.graticule()
+    pyplot.savefig('test_images/real_smooth_variance_map.png')
+    pyplot.close()
+
+    pyplot.figure(figsize=(8,16))
+    hp.mollview(II, title='input_II',sub=(3,1,1))
+    hp.mollview(QQ, title='input_QQ',sub=(3,1,2))
+    hp.mollview(UU, title='input_UU',sub=(3,1,3))
+    hp.graticule()
+    pyplot.savefig('test_images/real_input_variance_map.png')
+    pyplot.close()
+
+
+
 if __name__ == "__main__":
     np.random.seed(0)   
     os.makedirs('test_images',exist_ok=True)
-    test_produce_dec_mask()
-    test_square_beam()
-    test_smooth_map()
-    test_smooth_variance_map()
+    #test_produce_dec_mask()
+    #test_square_beam()
+    #test_smooth_map()
+    test_smooth_real_variance_map()
     print("All tests passed!")